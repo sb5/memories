25ffaf65100384de97619c49939f320f7d2a60e7
sb main <steveb533@hotmail.com>
2013-06-13 20:44:38 -0400

    message

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'lso7erHgaE8uKbLZaOZow');
+define('CONSUMER_SECRET', '3BnpB8o14z6oZ4mMh7mYohBMVM32LeVi9AYzAbOktM');
+define('OAUTH_CALLBACK', 'http://www.mytwittermemories.com/callback.php');
